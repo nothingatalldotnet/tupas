c4093685b94fd0af521c5f86f73918d9efce7395
rich <rich@nothingatall.net>
2019-04-24 22:09:17 +0100

    CPTs and bits

--- wp-content/themes/tupas/functions/functions-cpt.php
+++ wp-content/themes/tupas/functions/functions-cpt.php
@@ -13,3 +13,89 @@
         remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
     }
     add_action('wp_dashboard_setup', 'remove_draft_widget', 999);
+
+
+    // post types
+    function create_post_type() {
+        register_post_type('News',
+            array(
+                'labels' => array(
+                    'name' => __('News'),
+                    'singular_name' => _('News')
+                ),
+                'show_in_nav_menus' => true,
+                'menu_position'=> 20,
+                'menu_icon' => 'dashicons-megaphone',
+                'public' => true,
+                'has_archive' => true,
+                'query_var' => true,
+                'supports' => array('title','revisions'),
+                'rewrite' => array(
+                    'slug'  => 'news',
+                    'pages' => true,
+                    'with_front' => true
+                ),
+            )
+        );
+        register_post_type('Blog',
+            array(
+                'labels' => array(
+                    'name' => __('Blog'),
+                    'singular_name' => _('Blog')
+                ),
+                'show_in_nav_menus' => true,
+                'menu_position'=> 21,
+                'menu_icon' => 'dashicons-admin-post',
+                'public' => true,
+                'has_archive' => true,
+                'query_var' => true,
+                'supports' => array('title','revisions'),
+                'rewrite' => array(
+                    'slug'  => 'blog',
+                    'pages' => true,
+                    'with_front' => true
+                ),
+            )
+        );
+        register_post_type('Articles',
+            array(
+                'labels' => array(
+                    'name' => __('Articles'),
+                    'singular_name' => _('Article')
+                ),
+                'show_in_nav_menus' => true,
+                'menu_position'=> 22,
+                'menu_icon'=>'dashicons-align-left',
+                'public' => true,
+                'has_archive' => true,
+                'query_var' => true,
+                'supports' => array('title','revisions'),
+                'rewrite' => array(
+                    'slug'  => 'articles',
+                    'pages' => true,
+                    'with_front' => true
+                ),
+            )
+        );
+        register_post_type('Gallery',
+            array(
+                'labels' => array(
+                    'name' => __('Gallery'),
+                    'singular_name' => _('Gallery')
+                ),
+                'show_in_nav_menus' => true,
+                'menu_position'=> 23,
+                'menu_icon'=>'dashicons-camera',
+                'public' => true,
+                'has_archive' => true,
+                'query_var' => true,
+                'supports' => array('title','revisions'),
+                'rewrite' => array(
+                    'slug'  => 'gallery',
+                    'pages' => true,
+                    'with_front' => true
+                ),
+            )
+        );
+    }
+    add_action('init','create_post_type');
--- wp-content/themes/tupas/functions/functions-menus.php
+++ wp-content/themes/tupas/functions/functions-menus.php
@@ -1,6 +1,6 @@
 <?php
 	function nav_menus() {
 		register_nav_menu('main-menu',__('Main Menu'));
-		register_nav_menu('footer-menu',__('Footer Menu Row'));
+		register_nav_menu('footer-menu',__('Footer Menu'));
 	}
 	add_action('init', 'nav_menus');
